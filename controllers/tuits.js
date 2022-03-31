cbdd600f872a5858f1d45cb9f474af448797121b
stanat23 <wang.zun@northeastern.edu>
2022-03-30 22:14:55 -0400

    first commit

---
+++ controllers/tuits.js
@@ -0,0 +1,16 @@
+export default [
+    {
+        "_id": "123",
+        "postedBy": {"username": "NASA"},
+        "tuit": "In 2021, our @NASAPersevere Mars rover landed",
+        "likes": 345,
+        "dislikes": 10
+    },
+    {
+        "_id": "234",
+        "postedBy": { "username": "NASA" },
+        "tuit": "Ingenuity helicopter takes flight.",
+        "likes": 456,
+        "dislikes": 20
+    }
+];
